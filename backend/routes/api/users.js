967bc8150135367066f7ad550daf6a58f3f6d76f
ChrisEkeDev <iam@chriseke.com>
2023-08-29 13:53:23 -0500

    Done: Auth Backend

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -15,7 +15,7 @@ const validateSignUp = [
     check('email').exists({ checkFalsy: true }).isEmail().withMessage('Please provide a valid email.'),
     check('password').exists({ checkFalsy: true }).isLength({ min: 6 }).withMessage('Password must be 6 characters or more.'),
     handleValidationErrors
-  ];
+];
 
 router.post('/', validateSignUp, async(req, res) => {
     const { firstName, lastName, email, password } = req.body;
